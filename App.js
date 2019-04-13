7627957e800fd42749939a02119c362c924e5e2e
thembones79 <michal.szumnarski@gmail.com>
2019-04-13 14:41:26 +0200

    added styling

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ import {
 import { Permissions, Notifications } from "expo";
 
 const PUSH_REGISTRATION_ENDPOINT = "http://generated-ngrok-url/token";
-const MESSAGE_ENDPIONT = "http://generated-ngrok-url/message";
+const MESSAGE_ENDPOINT = "http://generated-ngrok-url/message";
 
 export default class App extends React.Component {
   state = {
@@ -51,9 +51,23 @@ export default class App extends React.Component {
     this.setState({ notification });
   };
 
-  handleChangeText = (text) =>{
-    this.setState({messageText:text});
-  }
+  handleChangeText = text => {
+    this.setState({ messageText: text });
+  };
+
+  sendMessage = async () => {
+    fetch(MESSAGE_ENDPOINT, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        message: this.state.messageText
+      })
+    });
+    this.setState({ messageText: "" });
+  };
 
   componentDidMount() {
     this.registerForPushNotificationsAsync();
@@ -79,8 +93,26 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#474747",
     alignItems: "center",
     justifyContent: "center"
+  },
+  textInput: {
+    height: 50,
+    width: 300,
+    borderColor: "#f6f6f6",
+    borderWidth: 1,
+    backgroundColor: "#fff",
+    padding: 10
+  },
+  button: {
+    padding: 10
+  },
+  buttonText: {
+    fontSize: 18,
+    color: "#fff"
+  },
+  label: {
+    fontSize: 18
   }
 });
