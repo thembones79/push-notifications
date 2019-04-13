59e365ec68f38e2670a8a8ac26214fca28def23e
thembones79 <michal.szumnarski@gmail.com>
2019-04-13 14:23:15 +0200

    addednotification register function

--- App.js
+++ App.js
@@ -1,11 +1,76 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Modal,
+  TouchableOpacity
+} from "react-native";
+import { Permissions, Notifications } from "expo";
+
+const PUSH_REGISTRATION_ENDPOINT = "http://generated-ngrok-url/token";
+const MESSAGE_ENDPIONT = "http://generated-ngrok-url/message";
 
 export default class App extends React.Component {
+  state = {
+    notification: null,
+    messageText: ""
+  };
+
+  registerForPushNotificationsAsync = async () => {
+    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    if (status !== "granted") {
+      return;
+    }
+    let token = await Notifications.getExpoPushTokenAsync();
+
+    return fetch(PUSH_REGISTRATION_ENDPOINT, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        token: {
+          value: token
+        },
+        user: {
+          username: "warly",
+          name: "Dan Ward"
+        }
+      })
+    });
+
+    this.notificationSubscription = Notifications.addListener(
+      this.handleNotification
+    );
+  };
+
+  handleNotification = notification => {
+    this.setState({ notification });
+  };
+
+  handleChangeText = (text) =>{
+    this.setState({messageText:text});
+  }
+
+  componentDidMount() {
+    this.registerForPushNotificationsAsync();
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <TextInput
+          value={this.state.messageText}
+          onChangeText={this.handleChangeText}
+          style={styles.textInput}
+        />
+        <TouchableOpacity style={styles.button} onPress={this.sendMessage}>
+          <Text style={styles.buttonText}>Send</Text>
+        </TouchableOpacity>
+        {this.state.notification ? this.renderNotification() : null}
       </View>
     );
   }
@@ -14,8 +79,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
